5c6a499e5c6d985b43829a02e7ae9758146e10f9
WonSeokChoi-Unicorn <guyver97@hanmail.net>
2021-02-06 14:57:40 +0900

    불필요로 삭제

--- TEST_UnicornSlack.py
+++
@@ -1,6 +0,0 @@
-from slacker import Slacker
-
-slack = Slacker('xoxb-1719146385936-1710633395777-eX8VvNaI5jEq0mvBnALwUf85')
-
-# Send a message to #stocktrading channel
-slack.chat.post_message('#stocktrading', 'Hello Unicorn!')
